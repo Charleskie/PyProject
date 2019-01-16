1767f246cd324230cf26e9107296c2cd8596e462
Charleskie <13531158704@163.com>
2019-01-16 11:19:20 +0800

    LSTM代码

---
+++ py/predict/oneStation.py
@@ -0,0 +1,192 @@
+# coding=utf-8
+
+###
+#create by william
+###
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+time_step = 192 # 192  # 时间步 （一天的数据量为192） ----优化这个参数貌似对结果没什么用
+rnn_unit = 30  # 30  # hidden layer units
+batch_size = 60  # 60   # 每一批次训练多少个样例
+input_size = 1  # 输入层维度
+output_size = 1  # 输出层维度
+lr = 0.0006  # 学习率-
+FLAG = 'test'   # train or test
+
+# ——————————————————导入数据——————————————————————
+f=open('F:\\datasets\\stationFlow\\sjzc0607_workday')
+df=pd.read_csv(f,header=None,names=['time','name','inflow','outflow'])     #读入客流数据
+data=np.array(df['inflow'])   #获取进站数据
+# plt.plot(data)
+# plt.show()
+
+
+normalize_data = (data - np.mean(data)) / np.std(data)  # 标准化
+normalize_data = normalize_data[:, np.newaxis]  # 增加维度
+
+data_x, data_y = [], []
+for i in range(len(normalize_data) - time_step - 1):
+    x = normalize_data[i:i + time_step]
+    y = normalize_data[i + time_step]  # 用一天预测下一个5min
+    data_x.append(x.tolist())
+    data_y.append(y.tolist())
+data_y = np.reshape(data_y, (-1, 1, 1))
+# 分训练集和测试集
+train_num = int(len(data_x)*0.9)
+train_x = data_x[0:train_num]
+train_y = data_y[0:train_num]
+test_x = data_x[train_num:]
+
+test_y = data_y[train_num:]
+
+
+# -----------------------
+# 换一个站点
+# f=open('E:\\datasets\\stationFlow\\bj0607_workday')
+# df=pd.read_csv(f,header=None,names=['time','name','inflow','outflow'])     #读入客流数据
+# data=np.array(df['inflow'])   #获取进站数据
+# plt.plot(data)
+# plt.savefig('bj_row.jpg', dpi=1000, bbox_inches='tight')
+# # plt.show()
+#
+#
+# normalize_data = (data - np.mean(data)) / np.std(data)  # 标准化
+# normalize_data = normalize_data[:, np.newaxis]  # 增加维度
+#
+# data_x, data_y = [], []
+# for i in range(len(normalize_data) - time_step - 1):
+#     x = normalize_data[i:i + time_step]
+#     y = normalize_data[i + time_step]  # 用一天预测下一个5min
+#     data_x.append(x.tolist())
+#     data_y.append(y.tolist())
+# data_y = np.reshape(data_y, (-1, 1, 1))
+# test_x = data_x[:192*10]
+# test_y = data_y[:192*10]
+
+
+
+
+# —————————————————定义神经网络变量————————————————
+X = tf.placeholder(tf.float32, [None, time_step, input_size])
+Y = tf.placeholder(tf.float32, [None, 1, output_size])
+# 输入层、输出层权重、偏置
+weights = {
+    'in': tf.Variable(tf.random_normal([input_size, rnn_unit])),
+    'out': tf.Variable(tf.random_normal([rnn_unit, 1]))
+}
+biases = {
+    'in': tf.Variable(tf.constant(1.0, shape=[rnn_unit, ])),
+    'out': tf.Variable(tf.constant(1.0, shape=[1, ]))
+}
+
+
+# —————————————————定义神经网络—————————————————
+def lstm(batch):
+    w_in = weights['in']
+    b_in = biases['in']
+    X_in = tf.reshape(X, [-1, input_size])  # 将X转换成2维,为了输入层'in'的输入
+    input_rnn = tf.matmul(X_in, w_in) + b_in
+    input_rnn = tf.reshape(input_rnn, [-1, time_step, rnn_unit])  # 将tensor转换回3维,作为lstm cell的输入
+    cell = tf.nn.rnn_cell.BasicLSTMCell(rnn_unit)
+    init_state = cell.zero_state(batch, dtype=tf.float32)
+    # output_rnn是记录lstm每个输出节点的结果，final_states是最后一个cell的结果
+    output_rnn, final_states = tf.nn.dynamic_rnn(cell, input_rnn, initial_state=init_state,dtype=tf.float32)
+    outputs = tf.unstack(tf.transpose(output_rnn, [1, 0, 2]))  # 作为输出层'out'的输入
+    w_out = weights['out']
+    b_out = biases['out']
+    pred = tf.matmul(outputs[-1], w_out) + b_out  # time_step只取最后一项
+    return pred, final_states
+
+
+# ——————————————————训练模型——————————————————
+def train_lstm():
+    global batch_size
+    pred, _ = lstm(batch_size)
+    # 损失函数
+    loss = tf.reduce_mean(tf.square(tf.reshape(pred, [-1, 1]) - tf.reshape(Y, [-1, 1])))
+    train_op = tf.train.AdamOptimizer(lr).minimize(loss)
+
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        start = 0
+        end = start + batch_size
+        for i in range(1000):
+            _, loss_ = sess.run([train_op, loss], feed_dict={X: train_x[start:end], Y: train_y[start:end]})
+            start += batch_size
+            end = start + batch_size
+            print(i, loss_)
+            if i % 100 == 0:
+                print("保存模型：", saver.save(sess, './savemodel_1/sjzc.ckpt'))
+            if end >= len(train_x):
+                start = 0
+                end = start + batch_size
+    writer = tf.summary.FileWriter("./nn_log", sess.graph)
+    writer.close()
+
+                # ————————————————预测模型————————————————————
+
+
+def prediction():
+    global test_y
+    pred, _ = lstm(len(test_x))  # 预测时只输入[test_batch,time_step,input_size]的测试数据
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        # 参数恢复
+        module_file = './savemodel_1/sjzc.ckpt'
+        saver.restore(sess, module_file)
+        # 取测试样本,shape=[test_batch,time_step,input_size]
+
+        # 打印一些变量
+        # test_pred,outputs,w_out,b_out = sess.run([pred,outputs,w_out,b_out], feed_dict={X: test_x})
+        # print('outputs[-1] shape',outputs[-1].shape )
+        # print('outputs[-1]')
+        # print(outputs[-1])
+        # print('w_out sharp ', w_out.shape)
+        # print('w_out')
+        # print(w_out)
+        # print('b_out sharp ', b_out.shape)
+        # print('b_out')
+        # print(b_out)
+
+        test_pred = sess.run(pred, feed_dict={X: test_x})
+
+
+        # --计算正确率
+        test_pred = np.reshape(test_pred, (-1))
+        test_y = np.reshape(test_y, (-1))
+        test_pred = test_pred*np.std(data)+np.mean(data)
+        test_y = test_y*np.std(data)+np.mean(data)
+
+
+        # Mean Absolute Error
+        print("平均绝对误差(MAE):")
+        mae = np.mean(np.abs(test_pred - test_y))
+        print(mae)
+        # Root Mean Square Error
+        print("均方根误差(RMSE):")
+        rmse = np.sqrt(np.mean(np.square(test_pred-test_y)))
+        print(rmse)
+        # mean absolute percentage error
+        print("相对百分误差(MAPE):")
+        mape = np.mean(np.abs(test_pred - test_y)/test_y)
+        print(mape)
+
+        # 以折线图表示结果
+        plt.figure()
+        plt.plot(range(len(test_y)), test_y, 'r-', label='real')
+        plt.plot(range(len(test_pred)), (test_pred), 'b-', label='pred')
+        plt.legend(loc=0)
+        plt.title('sjzc station prediction')
+        plt.show()
+        # plt.savefig('bj.jpg', dpi=1000, bbox_inches='tight')
+
+
+if __name__ == '__main__':
+    if FLAG == 'train':
+        train_lstm()
+    elif FLAG == 'test':
+        prediction()
---
+++ py/predict/preProcessing.py
@@ -0,0 +1,211 @@
+
+import pandas as pd
+import numpy as np
+import os
+import re
+from os import walk
+from datetime import datetime
+
+##------------------------------------------------------------------------------------------
+# # 每一次读取文件为df 再concat()
+# files = []
+# # walk会返回3个参数，分别是路径，目录list，文件list
+# for f, _, i in walk("E:\\datasets\\stationFlow\\IO201701_02"):
+#     for j in i:
+#         if re.match('part-',j):
+#             files.append(j)
+#
+# frames = []
+# # # read_csv() 默认设置为0，即表示第一行为列名,要添加列名的话需要设置为None,并添加names
+# for i in files:
+#     a = pd.read_csv("E:\\datasets\\stationFlow\\IO201701_02\\"+i,header=None,names=["time","name","inflow","outflow"],index_col=None,sep=",")
+#     frames.append(a)
+# df = pd.concat(frames)
+# df1 = df[df["name"] == "世界之窗"]
+# df1["time"] = df1["time"].str.slice(1,20)
+# df1["time"] = df1["time"].str.replace("[","")
+# df1["time"] =  df1["time"].str.replace("T"," ")
+# df1["outflow"] = df1["outflow"].str.replace("]","")
+# print(df1)
+
+
+#------------------------------------------------------------------------------------------
+#先合并文件 再读为df 再进行处理
+# ##
+# newPath = 'E:\\datasets\\stationFlow\\new\\new05_2'
+# oldPath = 'E:\\datasets\\stationFlow\\IO20170507_31'
+#
+# filelist=os.listdir(oldPath)
+# newfile=open(newPath,'w',encoding='utf-8') #保存的文件设置为utf-8
+# for item in filelist:
+#     if re.match('part-', item):
+#         for txt in open(oldPath+'\\'+item,'r',encoding='utf-8'): #读取文件是也需要设置utf-8
+#             newfile.write(txt)
+#
+# df = pd.read_csv(newPath,engine='python',header=None,names=['time','name','inflow','outflow'],index_col=None,sep=",",encoding='utf-8')
+# df1 = df[df["name"] == "世界之窗"]
+# # df1["time"] = df1["time"].str.slice(1,20)
+# ## df.iloc(,) 两个参数分别表示行和列, df1["time"] 都可以用df1.iloc[:,0] 替代
+# df1["time"] = df1["time"].str.replace("[","")
+# df1["time"] = df1["time"].str.replace("T"," ")
+# df1["outflow"] = df1["outflow"].str.replace("]","")
+# # print(df1)
+# # index = 0 不保存索引列，可设置 header,sep,columns 等
+# df1.to_csv("E:\\datasets\\stationFlow\\new\\sjzc05_2",encoding='utf-8',index=0,header=0)
+
+#
+# newPath = 'E:\\datasets\\stationFlow\\all'
+# oldPath = 'E:\\datasets\\stationFlow\\new'
+#
+# filelist=os.listdir(oldPath)
+# newfile=open(newPath,'w',encoding='utf-8') #保存的文件设置为utf-8
+# for item in filelist:
+#     if re.match('new', item):
+#         for txt in open(oldPath+'\\'+item,'r',encoding='utf-8'): #读取文件是也需要设置utf-8
+#             newfile.write(txt)
+#
+# df1 = pd.read_csv(newPath,engine='python',header=None,names=['time','name','inflow','outflow'],index_col=None,sep=",",encoding='utf-8')
+#
+# # df1["time"] = df1["time"].str.slice(1,20)
+# ## df.iloc(,) 两个参数分别表示行和列, df1["time"] 都可以用df1.iloc[:,0] 替代
+# df1["time"] = df1["time"].str.replace("[","")
+# df1["time"] = df1["time"].str.replace("T"," ")
+# df1["outflow"] = df1["outflow"].str.replace("]","")
+# # print(df1)
+# # index = 0 不保存索引列，可设置 header,sep,columns 等
+# df1.to_csv("E:\\datasets\\stationFlow\\all1",encoding='utf-8',index=0,header=0)
+#
+# # df1 = df[df["name"] == "世界之窗"]
+
+
+#-----------------------------------------------------------------------------------------------------
+## 过滤需要的记录---old
+
+#---------------
+# 用正则表达式 貌似不行
+# path = 'E:\\datasets\\stationFlow\\sjzc'
+# df = pd.read_csv(path,engine='python',header=None,names=['time','name','inflow','outflow'],encoding='utf-8', index_col=None)
+# # df1 = df.loc[df["time"].str.match("2017-01-0*"), ["time", "name", "inflow","outflow"]]
+# patt  ='2017-..-.. 00.*'
+# df1 = df[df["time"].str.match(patt)]
+# print(df1)
+
+#--------------
+# 老老实实用for 循环吧
+# path = 'E:\\datasets\\stationFlow\\sjzc'
+# dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m-%d %H:%M:%S')
+# # 将time 作为行的索引
+# df = pd.read_csv(path,engine='python',header=None,names=['time','name','inflow','outflow'],encoding='utf-8',parse_dates=['time'],
+#                  index_col='time',date_parser=dateparse)
+# # 6月份的工作日和周末
+# # workday = ['01','02','05','06','07','08','09','12','13','14','15','16','19','20','21','22','23','26','27','28','29','30'] #日期
+# # weekday = ['03','04','10','11','17','18','24','25']
+# # 7月份的工作日和周末
+# # workday = ['03','04','05','06','07','10','11','12','13','14','17','18','19','20','21','24','25','26','27','28','31'] #日期
+# # weekday = ['01','02','08','09','15','16','22','23','29','30']
+# # 8月份的工作日和周末
+# workday = ['03','04','05','06','07','10','11','12','13','14','17','18','19','20','21','24','25','26','27','28','31'] #日期
+# weekday = ['01','02','08','09','15','16','22','23','29','30']
+#
+# hour = ['07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22'] # 小时
+# frames = []
+# for i in workday:
+#     for j in hour:
+#         df1 = (df['2017-07-'+i+' '+j])
+#         frames.append(df1)
+# df2 = pd.concat(frames)
+# df2.to_csv("E:\\datasets\\stationFlow\\sjzc07",header = 0,encoding = 'utf-8')
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+##  过滤需要的记录---new
+
+
+## 提取时段，工作日/周末，节假日
+
+
+# path = 'F:\\datasets\\stationFlow\\allstation'
+# dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m-%d %H:%M:%S')
+# df = pd.read_csv(path,engine='python',header=None,names=['time','name','inflow','outflow'],encoding='utf-8',parse_dates=['time'],date_parser=dateparse)
+#
+# df['year'] = df['time'].apply(lambda x:x.year)
+# df['month'] = df['time'].apply(lambda x: x.month)
+# df['day'] = df['time'].apply(lambda x: x.day)
+# df['hour'] = df['time'].apply(lambda x:x.hour)
+# df['weekday'] = df['time'].apply(lambda x: x.weekday()+1) #x.weekday()返回日期的星期(0-6),周一为0
+## 取出每天7点到22点时间段
+# df = df[(df['hour']<=22) & (df['hour']>=7)]
+#将中间结果保存一下
+# df.to_csv("E:\\datasets\\stationFlow\\midResult",header=0,index=0,encoding='utf-8')
+
+#---------------------------------------------------------
+df = pd.read_csv('F:\\datasets\\stationFlow\\midResult',names=['time','name','inflow','outflow','year','month','day','hour','weekday'])
+
+## 过滤2017年的数据
+df = df[(df['year'] ==2017)]
+
+
+# 工作日：label=0   休息日：label=1
+isrest = ((df['weekday'] == 6) | (df['weekday'] == 7)) # 返回bool值
+df['label'] = isrest*1
+
+# 节假日:label=2
+holiday = ['01-01','01-02','01-03','01-27','01-28','01-29','01-30','01-31',
+           '02-01','02-02','04-03','04-04','04-05','05-01','05-02','05-03',
+           '05-28','05-29','05-30'] #2017年上半年假期
+for h in holiday:
+    #h = '01-01'
+    h_month,h_day = h.split('-')
+    h_index = ((df['month'] == int(h_month)) & (df['day'] == int(h_day)))
+    df.loc[h_index, 'label'] = 2
+
+## 过滤6、7月份工作日(0)、休息日（1）
+# df = df[(((df['month']==6) | (df['month']==7)) & (df['label']==0))]
+
+df = df[(df['label']==0)&(df['month']<=7)&(df['month']!=3)] # 上半年非工作日
+##站点
+station = df[df["name"] == "世界之窗"]
+station.to_csv("F:\\datasets\\stationFlow\\sjzc1-7_workday",header=0,index=0,encoding='utf-8',columns=['time','name','inflow','outflow'])
+
+
+# ## 线路
+# # 提取线路需要先将date转化成相同的区间
+# #读取静态数据
+staticPath = 'F:\\datasets\\stationFlow\\subway_zdbm_station_new'
+df1 = pd.read_csv(staticPath,names=['stationID','stationName','lineID','lineName','lat','lon'],encoding='utf-8')
+
+line =df1[df1['lineName'] =='地铁一号线']
+line = line.sort_values(by='stationID')
+name = np.array(line['stationName'])
+print(len(name))
+
+sta0 = df[df['name'] == name[0]].iloc[:,[0,2]]
+# sta1 = df[df['name'] == name[1]].iloc[:,2]
+# sta2 = df[df['name'] == name[2]].iloc[:,[0,2]]
+# sta0 = pd.merge(sta0,sta2,how='inner',on='time')
+
+for i in range(1,len(name)):
+    sta1 = df[df['name'] == name[i]].iloc[:,[0,2]]
+    sta0 = pd.merge(sta0,sta1,how='inner',on='time')
+
+# sta0.to_csv("E:\\datasets\\stationFlow\\line\\line2_0607",header=0,index=0,encoding='utf-8')
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------
+## 按天统计世界之窗的客流,并结合天气数据
+# df = pd.read_csv('E:\\datasets\\stationFlow\\midResult',names=['time','name','inflow','outflow','year','month','day','hour','weekday'])
+# df = df[(df['year'] ==2017)]
+# df = df[df["name"] == "世界之窗"]
+# df = df.groupby(by=['month','day'])['outflow'].sum()
+# df = pd.DataFrame(df)
+#
+# dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m-%d')
+# df1 = pd.read_csv('E:\MyProject_siat\Python\PaChong\crawle_weather\shenzhen201701-201712',parse_dates=['data'],date_parser=dateparse)
+#
+# df1['year'] = df1['data'].apply(lambda x:x.year)
+# df1['month'] = df1['data'].apply(lambda x: x.month)
+# df1['day'] = df1['data'].apply(lambda x: x.day)
+#
+# df2 = pd.merge(df,df1,how='inner',on=['month','day'])
+#
+# df2.to_csv("C:\\Users\\will\\Desktop\\weatherOutflow",header=0,index=0,columns = ['data','high','low','weather','winddirect','outflow'])
